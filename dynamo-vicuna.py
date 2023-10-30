0567a15b12ef060c12d79e32d77f89656b221cc7
followtheart <followtheart@outlook.com>
2023-10-30 23:32:47 +0800

    fix torch.jit.trace(vicuna,...)

--- dynamo-vicuna.py
+++ dynamo-vicuna.py
@@ -13,7 +13,6 @@ torch.ops.load_library("./libmpi_extensions.so")
 
 def get_gpu_memory(max_gpus=None):
     """Get available memory for each GPU."""
-    import torch
 
     gpu_memory = []
     num_gpus = (
@@ -33,7 +32,7 @@ def get_gpu_memory(max_gpus=None):
     return gpu_memory
 
 
-num_gpus = 2
+num_gpus = 1
 max_gpu_memory = None
 kwargs = {"torch_dtype": torch.float16}
 if num_gpus != 1:
@@ -58,9 +57,10 @@ model = AutoModelForCausalLM.from_pretrained(
     model_path,
     torchscript=True,
     low_cpu_mem_usage=True,
-    **kwargs
+    # **kwargs
 )
-
+device = "cpu"
+model = model.to(device)
 tokenizer = AutoTokenizer.from_pretrained(model_path)
 DUMMY_TEXT = "Girafatron is obsessed with giraffes, the most glorious animal on the face of "
 tok_text = tokenizer.tokenize(DUMMY_TEXT)
@@ -68,7 +68,7 @@ tokens = tokenizer.encode(DUMMY_TEXT)
 tok_arr = np.asarray(tokens).reshape(1, -1).astype(np.int32)
 tok_arr = np.pad(tok_arr, [(0, 0), (0, 128-tok_arr.shape[1])])
 tok_arr = np.repeat(tok_arr, 2, axis=0)
-tokens_tensor = torch.from_numpy(tok_arr).to('cuda')
+tokens_tensor = torch.from_numpy(tok_arr).to(device)
 print(tokens_tensor.shape)
 # input_ids = inputs.input_ids
 
@@ -80,7 +80,8 @@ def linear_sharding(linear: torch.nn.Linear = None, group=[], split_output: bool
         if split_output:
             '''拆分output'''
             sub_linear = torch.nn.Linear(linear.in_features, len(group[i]))
-            sub_linear.weight = torch.nn.Parameter(linear.weight[group[i], :])
+            sub_linear.weight = torch.nn.Parameter(torch.squeeze(linear.weight[group[i], :]))
+            print(sub_linear.weight.shape)
             if linear.bias != None:
                 sub_linear.bias = torch.nn.Parameter(linear.bias[group[i]])
         else:
@@ -93,7 +94,7 @@ def linear_sharding(linear: torch.nn.Linear = None, group=[], split_output: bool
     return result
 
 
-nrank = 4
+nrank = 2
 sub_graphs = []
 
 
@@ -109,6 +110,12 @@ def generate_group(total_features=0, nranks=4):
 
 sub_graphs = []
 
+def fix_arg(node:torch.fx.Node=None,replace_table:dict=dict()):
+    for i in range(len(node.args)):
+        for key, replacement in replace_table.items():
+            if node.args[i] == key:
+                node.update_arg(i, replacement)     
+    return node
 
 def stage_constructor(gm: torch.fx.GraphModule, example_inputs: List[torch.Tensor]):
     sub_graphs = [torch.fx.GraphModule(gm, torch.fx.Graph())
@@ -119,6 +126,7 @@ def stage_constructor(gm: torch.fx.GraphModule, example_inputs: List[torch.Tenso
     # mod.save('vicuna-7b-v1.5.pt')
     buffers = []
     params = []
+    replace_tables=[dict() for _ in range(nrank)]
     for name, _ in gm.named_buffers():
         buffers.append(name)
     for name, _ in gm.named_parameters():
@@ -127,30 +135,26 @@ def stage_constructor(gm: torch.fx.GraphModule, example_inputs: List[torch.Tenso
     last_node = {}
     for node in gm.graph.nodes:
         if node.op == 'call_module':
-            if isinstance(gm.get_submodule(node.target), torch.nn.Linear):
-                linear_mod = gm.get_submodule(node.target)
-                groups = generate_group(linear_mod.out_features, nrank)
-                linear_shards = linear_sharding(linear_mod, groups, True)
+            module = gm.get_submodule(node.target)
+            if isinstance(module, torch.nn.Linear):                
+                groups = generate_group(module.out_features, nrank)
+                linear_shards = linear_sharding(module, groups, True)
                 for i in range(nrank):
                     sub_graphs[i].add_submodule(node.target, linear_shards[i])
+                    sub_graphs[i].graph.node_copy(node)                    
                     new_node = sub_graphs[i].graph.call_function(
-                        torch.ops.mpi_extensions.tensor_concat, args=(node,))
-                node_added = True
-                last_node = new_node
+                        torch.ops.mpi_extensions.tensor_concat, args=(node,2))
+                    replace_tables[i][node] = new_node
                 print(node.name)
             else:
-                new_node = node
-                if node_added:
-                    new_node.update_arg(0, last_node)
                 for i in range(nrank):
+                    new_node = fix_arg(node,replace_tables[i])
                     sub_graphs[i].graph.node_copy(new_node)
+                    sub_graphs[i].add_submodule(node.target,module)
                 node_added = False
         elif node.op == 'get_attr':
-            new_node = node
-            if node_added:
-                new_node.update_arg(0, last_node)
-
             for i in range(nrank):
+                new_node = fix_arg(node,replace_tables[i])
                 if node.target in buffers:
                     sub_graphs[i].register_buffer(
                         node.target, gm.get_buffer(node.target))
@@ -158,17 +162,14 @@ def stage_constructor(gm: torch.fx.GraphModule, example_inputs: List[torch.Tenso
                     sub_graphs[i].register_parameter(
                         node.target, gm.get_parameter(node.target))
                 sub_graphs[i].graph.node_copy(new_node)
-            node_added = False
         else:
-            new_node = node
-            if node_added:
-                new_node.update_arg(0, last_node)
             for i in range(nrank):
+                new_node = fix_arg(node,replace_tables[i])
                 sub_graphs[i].graph.node_copy(new_node)
-            node_added = False
     for i in range(nrank):
         sub_graphs[i].recompile()
         sub_graphs[i].eval()
+        # sub_graphs[i].graph.print_tabular()
         mod = torch.jit.trace(sub_graphs[i], [tokens_tensor])
         mod.save('vicuna-7b-v1.5-'+str(i)+'.pt')
 
